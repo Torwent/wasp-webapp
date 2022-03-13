0a7f981659ec381eff6579dfb512905a4a2d6264
Torwent <dinisgrilo@gmail.com>
2022-03-13 14:36:55 +0100

    Made colors scheme not hard coded in most cases

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -9,7 +9,7 @@
 	let show = false // menu state
 </script>
 
-<nav class="relative px-2 sm:px-4 py-2.5 bg-orange-800">
+<nav class="relative px-2 sm:px-4 py-2.5 bg-accent">
 	<div class="max-w-6xl mx-auto px-4 top-0">
 		<div class="flex justify-between">
 			<div class="flex space-x-7">
@@ -17,30 +17,33 @@
 				<div>
 					<a href="/" class="flex items-center py-4 px-2">
 						<img src={logo} class="mr-3 h-8" alt="WaspBot Logo" />
-						<span class="font-bold text-white text-lg">WaspBot</span>
+						<span class="font-bold text-color1 text-lg">WaspBot</span>
 					</a>
 				</div>
 				<!-- Menu -->
-				<div class="hidden md:flex items-center space-x-1 text-gray-200">
+				<div class="hidden md:flex items-center space-x-1 text-color2">
 					<a
 						href="/"
-						class="py-4 px-2 text-white border-b-4 border-white transition duration-300 hover:shadow-sm"
+						class="py-4 px-2 text-color1 border-b-4 border-white transition duration-300 hover:shadow-sm"
 					>
 						Home
 					</a>
 					<a
 						href="/setup"
-						class="py-4 px-2 hover:text-white transition duration-300 hover:shadow-sm"
+						class="py-4 px-2 hover:text-color1 transition duration-300 hover:shadow-sm"
 					>
 						Setup
 					</a>
 					<a
 						href="/scripts"
-						class="py-4 px-2 hover:text-white transition duration-300 hover:shadow-sm"
+						class="py-4 px-2 hover:text-color1 transition duration-300 hover:shadow-sm"
 					>
 						Scripts
 					</a>
-					<a href="/faq" class="py-4 px-2 hover:text-white transition duration-300 hover:shadow-sm">
+					<a
+						href="/faq"
+						class="py-4 px-2 hover:text-color1 transition duration-300 hover:shadow-sm"
+					>
 						FAQ
 					</a>
 				</div>
@@ -66,7 +69,7 @@
 						<div
 							in:scale={{ duration: 100, start: 0.95 }}
 							out:scale={{ duration: 75, start: 0.95 }}
-							class="origin-top-right top-12 absolute right-0 w-80 py-10 bg-gray-700 mt-1 rounded-md shadow-lg container group max-w-sm items-center mx-auto content-div"
+							class="origin-top-right top-12 absolute right-0 w-80 py-10 bg-color1 mt-1 rounded-md shadow-lg container group max-w-sm items-center mx-auto content-div"
 						>
 							{#if $user}
 								<Logout />
