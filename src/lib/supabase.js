929419dc7952b4ca08a4efd9d78dc942b5f3d3b2
Torwent <dinisgrilo@gmail.com>
2022-05-29 08:37:34 +0200

    fix: attempt to fix user session not being persistent

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,14 @@
 import { createClient } from "@supabase/supabase-js"
 
+const options = {
+	autoRefreshToken: true,
+	persistSession: true
+}
+
 export const supabase = createClient(
 	import.meta.env.VITE_PUBLIC_SUPABASE_URL,
-	import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
+	import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
+	options
 )
 
 export const getServiceSupabase = () =>
