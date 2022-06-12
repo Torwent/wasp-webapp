3da96f1e44e0579c6eed4f0b619f21771ddddcc4
Torwent <dinisgrilo@gmail.com>
2022-06-12 08:29:06 +0200

    feat: finish the osrs wireframe interface

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -52,7 +52,7 @@
 
 		let mmX1 = canvas.width - 157
 		let mmY1 = 8
-		context.strokeRect(mmX1, mmY1, 151, 151)
+		//context.strokeRect(mmX1, mmY1, 151, 151)
 
 		let coords = [
 			[0, -76],
@@ -83,11 +83,57 @@
 			return result
 		}
 
-		let finalArr = offSetPoly(coords, [mmX1 + 151 / 2, mmY1 + 151 / 2])
+		let mmCenter = [mmX1 + 151 / 2, mmY1 + 151 / 2]
 
-		for (let i = 0; i < 7; i++) {
-			//context.strokeRect(tabsX1 + 6 + i * 33, tabsY1 + 1, 30, 33)
+		let finalArr = offSetPoly(coords, mmCenter)
+
+		context.beginPath()
+		for (let i = 0; i < finalArr.length; i++) {
+			context.moveTo(finalArr[i][0], finalArr[i][1])
+			if (i < finalArr.length - 1) {
+				context.lineTo(finalArr[i + 1][0], finalArr[i + 1][1])
+			} else {
+				context.lineTo(finalArr[0][0], finalArr[0][1])
+			}
 		}
+		context.stroke()
+
+		//Compass
+		context.beginPath()
+		context.arc(mmX1 - 2, mmY1 + 14, 17, 0, 2 * Math.PI)
+		context.stroke()
+
+		//xp
+		context.beginPath()
+		context.arc(mmX1 - 50, mmY1 + 32, 17, 0, 2 * Math.PI)
+		context.stroke()
+
+		//hp
+		context.beginPath()
+		context.arc(mmX1 - 13, mmY1 + 55, 17, 0, 2 * Math.PI)
+		context.stroke()
+
+		//prayer
+		context.beginPath()
+		context.arc(mmX1 - 13, mmY1 + 90, 17, 0, 2 * Math.PI)
+		context.stroke()
+
+		//Run
+		context.beginPath()
+		context.arc(mmX1 - 3, mmY1 + 121, 17, 0, 2 * Math.PI)
+		context.stroke()
+
+		//spec
+		context.beginPath()
+		context.arc(mmX1 + 19, mmY1 + 148, 17, 0, 2 * Math.PI)
+		context.stroke()
+	}
+
+	const drawXPBar = (canvas, context) => {
+		context.strokeStyle = "rgba(255, 255, 0, 1)"
+		context.strokeRect(canvas.width - 358 - 15, 0, 118, 28)
+
+		context.strokeRect(3, 3, 497, 18)
 	}
 
 	onMount(() => {
@@ -105,11 +151,13 @@
 			drawChatButtons(canvas, context)
 			drawGametabs(canvas, context)
 			drawMinimap(canvas, context)
+			drawXPBar(canvas, context)
 			//2 passes to remove all transparency.... there has to be a bette solution...
 			drawChatbox(canvas, context)
 			drawChatButtons(canvas, context)
 			drawGametabs(canvas, context)
 			drawMinimap(canvas, context)
+			drawXPBar(canvas, context)
 		}
 		resizeCanvas()
 	})
