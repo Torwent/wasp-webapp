5ef43636c64909add3231b08ddf9a35d82ea88d8
Torwent <dinisgrilo@gmail.com>
2022-07-10 19:13:43 +0200

    fix: added a link button to edit the current post

--- src/routes/blog/[slug]/index.svelte
+++ src/routes/blog/[slug]/index.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <script>
+	import { profile } from "$lib/stores/authStore"
 	import Markdown from "$lib/Markdown.svelte"
 	import { fade } from "svelte/transition"
 	export let post
@@ -21,6 +22,22 @@
 >
 	<h1 class="mb-4 font-bold text-3xl">{post.title}</h1>
 	<h2 class="font-semibold leading-normal mb-4">{post.description}</h2>
+
+	{#if $profile.id === "4dbcf43d-cc8a-48e3-aead-2c55a3f302ee"}
+		<div class="grid place-items-center">
+			<a href="/blog/{encodeURI(post.title)}/edit">
+				<button
+					data-mdb-ripple="true"
+					data-mdb-ripple-color="light"
+					class="px-6 py-2.5 text-white text-xs font-semibold leading-tight uppercase rounded shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out flex items-center justify-between 
+			bg-orange-500 hover:bg-orange-600 dark:bg-orange-400 dark:hover:bg-orange-500 my-2"
+				>
+					Edit
+				</button>
+			</a>
+		</div>
+	{/if}
+
 	<article class="prose dark:prose-invert py-6 border-t-2 border-stone-300 dark:border-stone-800">
 		<Markdown src={post.content} />
 	</article>
