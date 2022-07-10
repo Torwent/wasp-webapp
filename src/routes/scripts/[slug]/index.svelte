753261961ea9cb1651fb9cb8cc3573979e2dd5c3
Torwent <dinisgrilo@gmail.com>
2022-07-10 19:14:53 +0200

    fix: added an edit button for scripts

--- src/routes/scripts/[slug]/index.svelte
+++ src/routes/scripts/[slug]/index.svelte
@@ -104,6 +104,21 @@
 			{/if}
 		</div>
 
+		{#if $profile.id === script.id || $profile.id === "4dbcf43d-cc8a-48e3-aead-2c55a3f302ee"}
+			<div class="grid place-items-center">
+				<a href="/scripts/{encodeURI(script.title)}/edit">
+					<button
+						data-mdb-ripple="true"
+						data-mdb-ripple-color="light"
+						class="px-6 py-2.5 text-white text-xs font-semibold leading-tight uppercase rounded shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out flex items-center justify-between 
+			bg-orange-500 hover:bg-orange-600 dark:bg-orange-400 dark:hover:bg-orange-500 my-2"
+					>
+						Edit
+					</button>
+				</a>
+			</div>
+		{/if}
+
 		<article class="prose dark:prose-invert py-6 text-center">
 			<Markdown src={script.content} />
 		</article>
