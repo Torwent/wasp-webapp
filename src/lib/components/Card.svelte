e27422b95b58554d5dc6ba2e9035adb8ae953226
Torwent <dinisgrilo@gmail.com>
2022-08-25 17:08:02 +0200

    refactor: separated the card div itself from the the link in ScriptCards

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { fade } from "svelte/transition"
+	import EmojiTooltip from "$lib/components/EmojiTooltip.svelte"
+	import { categories, subcategories, loadData } from "$lib/stores/stores"
+
+	loadData("categories", categories)
+	loadData("subcategories", subcategories)
+
+	export let img: string
+	export let altImg: string = "Asset missing"
+	export let title: string
+	export let author: string
+	export let description: string
+	export let exportedCategories: string[]
+	export let exportedSubcategories: string[]
+
+	interface EmojiTooltip {
+		tooltip: string
+		icon: string
+	}
+
+	if (description.length > 80) {
+		description.substring(0, 80) + "..." //crop description at 80 characters.
+	}
+
+	let allCategories = [...$categories, ...$subcategories]
+	let tooltips: EmojiTooltip[] = []
+
+	const loadEmojis = (cats: string[]) => {
+		tooltips = []
+		for (let c of cats) {
+			for (let c2 of allCategories) {
+				if (c === c2.name) tooltips.push({ tooltip: c2.name, icon: c2.emoji })
+			}
+		}
+	}
+
+	$: if (
+		$categories.length > 0 &&
+		$subcategories.length > 0 &&
+		exportedCategories.length > 0 &&
+		exportedSubcategories.length > 0
+	)
+		loadEmojis([...exportedCategories, ...exportedSubcategories])
+
+	$: tooltips = tooltips
+</script>
+
+<div
+	class="flex flex-col shadow-md cursor-pointer duration-300 w-72 rounded-md
+		   hover:shadow-xl hover:shadow-black/20 hover:-translate-y-2"
+	in:fade={{ duration: 300, delay: 300 }}
+	out:fade={{ duration: 300 }}
+>
+	<!-- Preview -->
+	<div class="inline relative group h-48">
+		<!-- Thumbnail -->
+		<img class="absolute rounded-t object-cover h-full w-full" src={img} alt={altImg} />
+
+		<!-- Hover Effect -->
+		<div
+			class="flex flex-row absolute justify-end
+                    h-16 w-full bottom-0 px-3 space-x-2
+                    bg-none opacity-0 group-hover:opacity-100
+                    group-hover:bg-gradient-to-t from-black/20 via-gray-800/20 to-transparent 
+                    transition-all ease-in-out duration-200 delay-100"
+		/>
+	</div>
+	<!-- Body -->
+	<div class="flex flex-col bg-stone-100 dark:bg-stone-800 rounded-b p-3 h-[11.5rem]">
+		<!-- Title -->
+		<span class="text-md font-semibold text-amber-500 hover:underline truncate">
+			{title}
+		</span>
+
+		<!-- Author - Category -->
+		<span class="text-xs text-stone-400 truncate mt-1">
+			by
+
+			<!-- Author -->
+			<span class="font-semibold hover:underline">
+				{author}
+			</span>
+		</span>
+
+		<!-- content -->
+		<div class="text-sm text-stone-500 my-2 h-16 break-words overflow-clip">
+			{description}
+		</div>
+
+		<div class="grid grid-cols-12 mt-auto">
+			{#each tooltips as emoji}
+				<EmojiTooltip {emoji} />
+			{/each}
+		</div>
+	</div>
+</div>
--- src/lib/components/ScriptCard.svelte
+++ src/lib/components/ScriptCard.svelte
@@ -1,88 +1,26 @@
 <script lang="ts">
-	import { fade } from "svelte/transition"
-	import { categories, subcategories } from "$lib/stores/stores"
-	import EmojiTooltip from "$lib/components/EmojiTooltip.svelte"
+	import Card from "$lib/components/Card.svelte"
 	import type { Script } from "$lib/supabaseStorage"
+
 	export let script: Script
 
-	let allCategories = [...$categories, ...$subcategories]
-	let description =
-		script.description.length > 80
-			? script.description.substring(0, 80) + "..."
-			: script.description
+	let img =
+		"https://enqlpchobniylwpsjcqc.supabase.co/storage/v1/object/public/imgs/scripts/" +
+		script.id +
+		"/cover.jpg"
 
-	const loadEmojis = (cats: string[]) => {
-		let result = []
-		for (let c of cats) {
-			for (let c2 of allCategories) {
-				if (c === c2.name) {
-					result.push({ tooltip: c2.name, icon: c2.emoji })
-				}
-			}
-		}
-
-		return result
-	}
-
-	let assets_path =
-		"https://enqlpchobniylwpsjcqc.supabase.co/storage/v1/object/public/imgs/scripts/" + script.id
-
-	$: emojis = loadEmojis([...script.categories, ...script.subcategories])
+	let author = script.author != null ? script.author : ""
 </script>
 
 <!-- Product Card -->
 <a href="/scripts/{encodeURI(script.title) + '&' + script.id}">
-	<div
-		in:fade={{ duration: 300, delay: 300 }}
-		out:fade={{ duration: 300 }}
-		class="flex flex-col shadow-md hover:shadow-xl hover:shadow-black/20 cursor-pointer hover:-translate-y-2  duration-300 w-72 rounded-md"
-	>
-		<!-- Preview -->
-		<div class="inline relative group h-48">
-			<!-- Thumbnail -->
-
-			<img
-				class="absolute rounded-t object-cover h-full w-full"
-				src={assets_path + "/cover.jpg"}
-				alt={script.assets_alt}
-			/>
-
-			<!-- Hover Effect -->
-			<div
-				class="flex flex-row absolute justify-end
-                    h-16 w-full bottom-0 px-3 space-x-2
-                    bg-none opacity-0 group-hover:opacity-100
-                    group-hover:bg-gradient-to-t from-black/20 via-gray-800/20 to-transparent 
-                    transition-all ease-in-out duration-200 delay-100"
-			/>
-		</div>
-		<!-- Body -->
-		<div class="flex flex-col bg-stone-100 dark:bg-stone-800 rounded-b p-3 h-[11.5rem]">
-			<!-- Title -->
-			<div class="text-md font-semibold text-amber-500 hover:underline truncate">
-				{script.title}
-			</div>
-
-			<!-- Author - Category -->
-			<div class="text-xs text-stone-400 truncate mt-1">
-				by
-
-				<!-- Author -->
-				<a href="/user/{script.author}" class="font-semibold hover:underline">
-					{script.author}
-				</a>
-			</div>
-
-			<!-- content -->
-			<div class="text-sm text-stone-500 my-2 h-16 break-words overflow-clip">
-				{description}
-			</div>
-
-			<div class="grid grid-cols-12 mt-auto">
-				{#each emojis as emoji}
-					<EmojiTooltip {emoji} />
-				{/each}
-			</div>
-		</div>
-	</div>
+	<Card
+		{img}
+		altImg={script.assets_alt}
+		title={script.title}
+		{author}
+		description={script.description}
+		exportedCategories={script.categories}
+		exportedSubcategories={script.subcategories}
+	/>
 </a>
