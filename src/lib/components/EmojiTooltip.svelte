540e6824badc40e6e2dd3f693ea32f69099d2862
Torwent <dinisgrilo@gmail.com>
2022-08-30 18:47:17 +0200

    fix: tooltips now fade in/out instead of instantly popping up

--- src/lib/components/EmojiTooltip.svelte
+++ src/lib/components/EmojiTooltip.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { fade } from "svelte/transition"
 	export let emoji: { tooltip: string; icon: string }
 
 	let show = false
 </script>
 
-<div>
+<div in:fade={{ duration: 300, delay: 300 }} out:fade={{ duration: 300 }}>
 	{#if show}
 		<div
 			class="absolute bottom-12 px-4 py-2 bg-stone-200/90 dark:bg-stone-700/90 rounded-md z-40 overflow-visible"
