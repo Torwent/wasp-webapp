347abc4f4361de26c70f5b9f76937b11755a8f05
Torwent <Torwent@users.noreply.github.com>
2022-09-23 19:09:40 +0200

    Update +server.ts

--- src/routes/updateuser/[slug]/+server.ts
+++ src/routes/updateuser/[slug]/+server.ts
@@ -4,7 +4,7 @@ import { updateRoles } from "$lib/stores/authStore"
 import WebSocket from "ws"
 import { getData } from "$lib/database/supabase"
 
-const ws = new WebSocket("wss://waspscripts.com/wss")
+const ws = new WebSocket("ws://wasp-discord:4100")
 
 export const GET: RequestHandler = async ({ params }) => {
 	const { slug } = params
