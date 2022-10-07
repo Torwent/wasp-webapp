c406741387f7cca612754a9cd91cd54a654e48d7
Torwent <dinisgrilo@gmail.com>
2022-10-07 13:37:27 +0200

    fix: forgot to check if the user was logged in in the avatar component

--- src/routes/Avatar.svelte
+++ src/routes/Avatar.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import type { Profile } from "$lib/database/types"
-	import Loading from "$lib/components/Loading.svelte"
 	import { user, avatar, reloadAvatar, getProfile } from "$lib/stores/authStore"
 	const profilePromise = getProfile() as unknown as Profile
 	reloadAvatar()
 </script>
 
 <div class="flex place-items-center">
-	{#await profilePromise}
-		<span class="font-semibold place-items-center px-4">Loading</span>
-	{:then profile}
-		<span class="font-semibold place-items-center px-4">{profile.username}</span>
-	{/await}
+	{#if $user}
+		{#await profilePromise}
+			<span class="font-semibold place-items-center px-4">Loading</span>
+		{:then profile}
+			<span class="font-semibold place-items-center px-4">{profile.username}</span>
+		{/await}
+	{/if}
 	<div
 		class="rounded-full w-12 h-12 shadow-lg bg-amber-50 border-amber-600 hover:bg-white hover:border-amber-400 border-2 flex place-items-center"
 	>
