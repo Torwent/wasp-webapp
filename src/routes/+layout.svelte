3c7e6f2a056af16316c69df613848be0a3010f00
Torwent <Torwent@users.noreply.github.com>
2023-05-31 21:14:14 +0200

    fix: add banner to advertise the new site version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@
 	import Footer from "./Footer.svelte"
 </script>
 
+<div class="h-8 bg-amber-500 text-center">
+	<a href="https://dev.waspscripts.com" class="text-black hover:text-white"
+		>Try the new reworked website!</a
+	>
+</div>
 <Navbar />
 <main class="min-h-screen mx-8 sm:mx-12 md:mx-16 lg:mx-24">
 	<slot />
