8036bccd255854be2a8af1ab23284451c7b406aa
Torwent <Torwent@users.noreply.github.com>
2023-06-01 15:07:25 +0200

    fix: add redirect to hopefully fix subdomain login

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,12 +4,16 @@ import { fail, redirect } from "@sveltejs/kit"
 
 export const actions: Actions = {
 	login: async ({ locals, url }) => {
+		const redirectURL = url.href.replace(url.search, "")
 		const provider = url.searchParams.get("provider") as Provider
 
 		if (provider) {
 			const { data, error: err } = await locals.supabase.auth.signInWithOAuth({
 				provider: provider,
-				options: { scopes: "identify email guilds guilds.members.read" }
+				options: {
+					redirectTo: redirectURL,
+					scopes: "identify email guilds guilds.members.read"
+				}
 			})
 
 			if (err) return fail(400, { message: "Something went wrong logging you in!" })
@@ -19,7 +23,10 @@ export const actions: Actions = {
 	},
 
 	logout: async ({ locals, url }) => {
+		const redirectURL = url.href.replace(url.search, "")
 		const { error } = await locals.supabase.auth.signOut()
 		if (error) return fail(400, { message: "Something went wrong logging you out!" })
+
+		throw redirect(303, redirectURL)
 	}
 }
