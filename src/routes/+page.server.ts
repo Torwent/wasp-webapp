dcf65b6bc2106838edc7aa650322b7bc559be65a
Torwent <Torwent@users.noreply.github.com>
2023-06-01 15:44:27 +0200

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,15 +4,13 @@ import { fail, redirect } from "@sveltejs/kit"
 
 export const actions: Actions = {
 	login: async ({ locals, url }) => {
-		console.log(url)
-		const redirectURL = url.href.replace(url.search, "")
 		const provider = url.searchParams.get("provider") as Provider
 
 		if (provider) {
 			const { data, error: err } = await locals.supabase.auth.signInWithOAuth({
 				provider: provider,
 				options: {
-					redirectTo: redirectURL,
+					redirectTo: url.origin,
 					scopes: "identify email guilds guilds.members.read"
 				}
 			})
@@ -23,12 +21,8 @@ export const actions: Actions = {
 		}
 	},
 
-	logout: async ({ locals, url }) => {
-		console.log(url)
-		const redirectURL = url.href.replace(url.search, "")
+	logout: async ({ locals }) => {
 		const { error } = await locals.supabase.auth.signOut()
 		if (error) return fail(400, { message: "Something went wrong logging you out!" })
-
-		throw redirect(303, redirectURL)
 	}
 }
