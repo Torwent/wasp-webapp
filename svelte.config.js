36256718e9ec6c58b69f42af91cd1c7be1786ce1
Torwent <Torwent@users.noreply.github.com>
2023-07-01 20:44:38 +0200

    test: completely remove csp

--- svelte.config.js
+++ svelte.config.js
@@ -6,21 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		csp: {
-			mode: "auto",
-			directives: {
-				"script-src": [
-					"self",
-					"https://enqlpchobniylwpsjcqc.supabase.co/",
-					"unsafe-inline",
-					"sha256-PBmRLzA7Ofi7WtgbjS5vmS9t83AYv5oRRywasLtzZ9Y=", //dev
-					"sha256-k5HJr61/45h+hLBmZFy0IrJtHlGahgw6wXNW3ccZaAI=" //build
-				],
-				"base-uri": ["self"],
-				"object-src": ["none"]
-			}
-		}
+		adapter: adapter()
 	}
 }
 
