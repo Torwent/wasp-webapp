26ee803ce473efb06a3a16118ab00e6047398315
Torwent <Torwent@users.noreply.github.com>
2023-08-17 02:07:51 +0200

    fix: script cards oops

--- src/lib/components/ScriptCard.svelte
+++ src/lib/components/ScriptCard.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import type { Script } from "$lib/types/collection"
-	import { encodeSEO } from "$lib/utils"
 	import ScriptCardBase from "./ScriptCardBase.svelte"
 	export let script: Script
-
-	let link = "/scripts/" + encodeSEO(script.title + " by " + script.protected.username)
-	$: link = "/scripts/" + encodeSEO(script.title + " by " + script.protected.username)
+	let link = "/scripts/" + script.url
+	$: link = "/scripts/" + script.url
 </script>
 
 <a href={link}>
