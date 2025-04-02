20547631f88acfead4168db377a54a5966c3a4c5
Torwent <Torwent@users.noreply.github.com>
2025-04-02 21:37:11 +0200

    fix: disable client sided code highlighter

--- src/lib/client/markdown.ts
+++ src/lib/client/markdown.ts
@@ -3,15 +3,15 @@ import Shiki from "@shikijs/markdown-it"
 import { full as emoji } from "markdown-it-emoji"
 import { imgLazyload } from "@mdit/plugin-img-lazyload"
 
-const shikiHighlighter = await Shiki({
+/* const shikiHighlighter = await Shiki({
 	themes: { light: "github-light", dark: "github-dark" },
 	langs: ["javascript", "typescript", "bash", "cmd", "yml", "yaml", "pascal", "java"]
-})
+}) */
 
 export const mardownRenderer = new markdownit("commonmark", {
 	linkify: true,
 	typographer: true
 })
-	.use(shikiHighlighter)
+	//	.use(shikiHighlighter)
 	.use(emoji)
 	.use(imgLazyload)
