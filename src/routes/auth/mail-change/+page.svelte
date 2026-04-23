2382fd39535fe8193fd98090db755c851f7d518b
Torwent <Torwent@users.noreply.github.com>
2026-04-23 19:51:49 +0200

    fix: re-add the deleted mail-change page oops

---
+++ src/routes/auth/mail-change/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { data } = $props()
+	const { message } = data
+</script>
+
+<main class="container mx-auto my-44 max-w-2xl flex-grow">
+	<h3 class="my-4 flex font-bold">Email update message:</h3>
+	<p class="my-16">{message}</p>
+	<p class="my-16">Make sure you click the confirmation link on both emails 😁</p>
+</main>
