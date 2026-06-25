ce31fdabde463931cfb947217a56dd85869cc052
Torwent <Torwent@users.noreply.github.com>
2026-06-25 21:00:19 +0100

    fix: add security headers

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,16 @@
-export const load = async ({ locals: { safeGetSession }, cookies }) => {
+export const load = async ({ locals: { safeGetSession }, cookies, setHeaders }) => {
 	const darkMode = cookies.get("darkMode") === "true"
 	const theme = cookies.get("theme") ?? "wasp"
 	const { session } = await safeGetSession()
+
+	setHeaders({
+		"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+		"X-Frame-Options": "SAMEORIGIN",
+		"X-Content-Type-Options": "nosniff",
+		"Referrer-Policy": "origin-when-cross-origin",
+		"Permissions-Policy":
+			"geolocation=(self), microphone=(self), camera=(self), fullscreen=(self), payment=(self), midi=(self)"
+	})
 
 	return {
 		darkMode,
